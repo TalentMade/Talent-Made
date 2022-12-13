1556f5c3ef7c967e3e300802bde4da61ebf836ef
Andy Luu <luuandyf2nd@gmail.com>
2022-12-12 21:49:10 -0800

    Updated API to pull user info

--- pages/api/userdatacontext/index.ts
+++ pages/api/userdatacontext/index.ts
@@ -6,9 +6,27 @@ const supabaseUrl = 'https://eiffdggkcjegchiecaje.supabase.co'
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+interface User {
+    id: number,
+    FirstName: string,
+    LastName: string,
+    CreateDate: string,
+    email: string,
+    password: string,
+    skills: UserSkills[]
+}
+
+interface UserSkills {
+    description: Skills
+}
+
+interface Skills {
+    skill: string,
+}
+
 
 export default function handler(req: NextApiRequest, res:NextApiResponse ){
-    
+
     if(req.method == "POST"){
         InsertNewUser(req, res);
     }
@@ -16,6 +34,13 @@ export default function handler(req: NextApiRequest, res:NextApiResponse ){
     if(req.method == "GET"){
         GetUser(req, res);
     }
+}
+
+async function getUserSkills(user_id: number) {
+    console.log("user id: ", user_id);
+    const result = await supabase.from("UserSkills").select('*').eq("user_id", user_id);
+    console.log("user skills result", result);
+    return result;
 }
 
 async function InsertNewUser(req: NextApiRequest, res:NextApiResponse){
@@ -35,11 +60,13 @@ async function InsertNewUser(req: NextApiRequest, res:NextApiResponse){
 }
 
 async function GetUser(req: NextApiRequest, res:NextApiResponse){
-
     console.log(req.body.username);
-    const result = await supabase.from("User").select("*").eq("UserName", req.body.username);
-
-    res.json(result);
+    const result = await supabase.from("User").select("*").eq("email", req.body.email);
+    const current_user: User = result.data?.[0] as User
+    const skills_result = await supabase.from("UserSkills").select(`description:Skills(skill)`).eq("user_id", current_user.id);
+    const current_skills: UserSkills[] = skills_result?.data as UserSkills[];
+    current_user.skills = current_skills;
+    res.json(current_user);
 }
 
 async function Update(req: NextApiRequest, res:NextApiResponse){
